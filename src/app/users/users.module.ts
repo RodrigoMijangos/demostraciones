64a0cb341430f183259e0971cff6eb18ececb24b
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-17 12:13:06 -0600

    add: se inicia componente de formulario de usuarios

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
+import { UsersFormComponent } from './users-form/users-form.component';
 
 
 
 @NgModule({
   declarations: [
-    UsersDashboardComponent
+    UsersDashboardComponent,
+    UsersFormComponent
   ],
   imports: [
     CommonModule
