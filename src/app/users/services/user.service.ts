593cb1768fa26d8b9fdef99acdf9f198ce91f082
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-17 14:31:21 -0600

    add: servicio de API para usuarios completo

---
+++ src/app/users/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserSerialization } from '../models/interfaces/user-serialization';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private base_url: string = "https://jsonplaceholder.typicode.com/users/"
+
+  constructor(private _http: HttpClient) { }
+
+  getAllUsers(): Observable<UserSerialization[]>{
+    return this._http.get<UserSerialization[]>(this.base_url);
+  }
+
+}
