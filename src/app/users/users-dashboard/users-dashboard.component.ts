024d00ed82c5e5712962e8156416d79319cf8272
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-17 12:09:39 -0600

    add: dashboard de usuarios básico terminado

--- src/app/users/users-dashboard/users-dashboard.component.ts
+++ src/app/users/users-dashboard/users-dashboard.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserSerialization } from '../models/interfaces/user-serialization';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-users-dashboard',
   templateUrl: './users-dashboard.component.html',
   styleUrl: './users-dashboard.component.css'
 })
-export class UsersDashboardComponent {
+export class UsersDashboardComponent implements OnInit{
+
+  users: UserSerialization[] = []
+
+  constructor(private _service: UserService){  }
+
+  ngOnInit(): void {
+      this._service.getAllUsers().subscribe(
+        response => this.users = response
+      )
+  }
+  
 
 }
