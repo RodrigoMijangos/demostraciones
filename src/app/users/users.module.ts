6fdbe6891c0f635c7363ca2e08ac7eac520e8458
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-17 14:43:50 -0600

    add: se añade módulo de formulario de usuario

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersDashboardComponent } from './users-dashboard/users-dashboard.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 
 
 @NgModule({
   declarations: [
-    UsersDashboardComponent
+    UsersDashboardComponent,
+    UserFormComponent
   ],
   imports: [
     CommonModule
