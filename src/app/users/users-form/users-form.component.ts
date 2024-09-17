1ef63adc50be88b3c9c20dcacc81508b05ef8965
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-17 10:59:58 -0600

    add: componente formulario de usuarios completo

--- src/app/users/users-form/users-form.component.ts
+++ src/app/users/users-form/users-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { UserSerialization } from '../models/interfaces/user-serialization';
 
 @Component({
   selector: 'app-users-form',
   templateUrl: './users-form.component.html',
   styleUrl: './users-form.component.css'
 })
-export class UsersFormComponent {
+export class UsersFormComponent implements OnDestroy{
+
+  @Output() emitter = new EventEmitter<UserSerialization>()
+
+  user: UserSerialization = {
+    id: 0,
+    name: "",
+    username: "",
+    phone: "",
+    website: ""
+  }
+
+  ngOnDestroy(): void {
+      console.log("El componente se destruye")
+  }
+
+  send_user(): void {
+    this.emitter.emit(this.user)
+  }
+
+  cancel(): void {
+    this.user.id = -1
+    this.emitter.emit(this.user)
+  }
 
 }
