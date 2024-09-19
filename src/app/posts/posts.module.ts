40cf300824596403d33cd8c0080c6bd7172a75d2
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-19 12:59:11 -0600

    add: se inicializa componente card para los posts

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPostsComponent } from './dashboard-posts/dashboard-posts.component';
+import { CardPostComponent } from './card-post/card-post.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardPostsComponent
+    DashboardPostsComponent,
+    CardPostComponent
   ],
   imports: [
     CommonModule
