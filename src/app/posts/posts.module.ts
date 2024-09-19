a17a23aa5e6e53ba3138d9129168eec85f9352a8
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-19 12:57:54 -0600

    add: se inicia componente dashboard para posts

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardPostsComponent } from './dashboard-posts/dashboard-posts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardPostsComponent
+  ],
   imports: [
     CommonModule
   ]
