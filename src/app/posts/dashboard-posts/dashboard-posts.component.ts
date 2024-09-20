cdb8994a93612c8f923dbafd193dcf91ae431465
Rodrigo Mijangos <rdrgmijangos@gmail.com>
2024-09-20 12:26:35 -0600

    add: se inicia componente dashboard posts

---
+++ src/app/posts/dashboard-posts/dashboard-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-posts',
+  templateUrl: './dashboard-posts.component.html',
+  styleUrl: './dashboard-posts.component.css'
+})
+export class DashboardPostsComponent {
+
+}
